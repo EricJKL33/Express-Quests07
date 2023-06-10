3e6bb4bb8ffe77c20de61a40e25a5944ac5edc88
EricJKL33 <eric.jkl@icloud.com>
2023-06-10 15:24:33 +0200

    exofini

--- app.js
+++ app.js
@@ -1,36 +1,65 @@
 require("dotenv").config();
+
 const express = require("express");
-const port = process.env.PORT;
+const movieHandlers = require("./movieHandlers.js");
+const userHandlers = require("./userHandlers.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+
 const app = express();
+
 app.use(express.json());
 
+const port = process.env.APP_PORT ?? 3000;
+
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Hello there!");
 };
 
 app.get("/", welcome);
 
-const userHandler = require("./usersHandlers");
-const movieHandlers = require("./movieHandlers");
-const { validateMovie, validateUser } = require("./validators");
-const { hashPassword } = require("./authenticate");
+/* ------------------------------ PUBLIC ROUTE --------------------------------- */
+// --------------------- MOVIES --------------------
 
-
-app.get("/api/users", userHandler.getUsers);
-app.get("/api/users/:id", userHandler.getUserById);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-app.post("/api/users", hashPassword, userHandler.postUsers);
-app.post("/api/movies", movieHandlers.postMovies)
-app.post("/api/movies", validateMovie, movieHandlers.postMovies);
-app.post("/api/users", validateUser, userHandler.postUsers);
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
 
-app.put("/api/movies/:id",validateMovie, movieHandlers.updateMovies);
-app.put("/api/users/:id", hashPassword, validateUser, userHandler.updateUsers);
+// --------------------- USERS --------------------
 
-app.delete("/api/movies/:id", movieHandlers.removeMovies);
-app.delete("/api/users/:id", userHandler.removeUsers);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+// --------------------- LOGIN --------------------
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+/* -------------------------------  PROTECT ROUTE -------------------------------- */
+
+app.use(verifyToken);
+
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+app.put(
+  "/api/users/:id",
+  (req, res, next) => {
+    if (req.body.password) {
+      hashPassword(req, res, next);
+    } else {
+      next();
+    }
+  },
+  userHandlers.updateUser
+);
+
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
--- database.js
+++ database.js
@@ -1,13 +1,22 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
 
-// CONFIG 
+// CONFIG
 const database = mysql.createPool({
-  host : process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST, // server address
+  port: process.env.DB_PORT, // (mysql) DB server PORT, not to be mistaken with express app PORT !
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+
+database
+  .getConnection()
+  .then(() => {
+    console.log("datatbase reached!");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 module.exports = database;
